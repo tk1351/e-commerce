9fc4623b486db4690f63902b41045756f41c9d59
tk1351 <takakikitsui0111@gmail.com>
2024-07-10 09:27:12 +0900

    feat: privacy policy page

---
+++ client/app/(site)/privacy-policy/page.tsx
@@ -0,0 +1,20 @@
+export default function Page() {
+  return (
+    <>
+      <h1 className="mb-8 text-5xl font-bold">Privacy Policy</h1>
+      <div className="mx-auto mb-8 max-w-6xl text-base leading-7 text-black dark:text-white">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nam libero justo
+        laoreet sit amet cursus sit. Dictumst quisque sagittis purus sit amet
+        volutpat consequat. Egestas diam in arcu cursus euismod. Sed faucibus
+        turpis in eu mi bibendum. Consectetur libero id faucibus nisl. Quisque
+        id diam vel quam elementum. Eros donec ac odio tempor orci dapibus
+        ultrices. Turpis tincidunt id aliquet risus. Pellentesque eu tincidunt
+        tortor aliquam nulla facilisi cras fermentum odio.
+      </div>
+      <p className="text-sm italic">
+        This document was last updated on 2024-07-10.
+      </p>
+    </>
+  );
+}
