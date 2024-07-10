4b3da7bcc3affb7aa000335c33734b8849e940ac
tk1351 <takakikitsui0111@gmail.com>
2024-07-10 09:21:38 +0900

    feat: site layout and terms and conditions page

---
+++ client/app/(site)/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export default function SiteLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="w-full">
+      <div className="mx-8 max-w-2xl py-20 sm:mx-auto">{children}</div>
+    </div>
+  );
+}
